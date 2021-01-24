f99a1e0ef42663633d178a50dc4cec2bfc6721be
Rashmi <monajain99@hotmail.com>
2021-01-24 14:06:20 -0500

    sideNav

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -8,14 +8,19 @@ import { Row, Col, Container } from "react-bootstrap"
 import SearchInput from "./Search";
 import Logo from "./images/logo.png"
 
-
 const NavBar = ({ setAuthenticated, authenticated, user }) => {
   let location = useLocation();
   const currentPage = location.pathname;
   return (
     <Container>
+      <div class="vertical-menu">
+        <a href="#" class="active">Portfolio</a>
+        <a href="#">Home</a>
+      </div>
       <Row>
         <Col>
+          <div className="middle">{authenticated && <SearchInput />}</div>
+
           <NavLink to="/" exact={true} activeClassName="active">
             <img
               src={Logo}
@@ -44,7 +49,16 @@ const NavBar = ({ setAuthenticated, authenticated, user }) => {
                 </NavLink>
               </li>
             )}
-            <div className="middle">{authenticated && <SearchInput />}</div>
+
+            {authenticated ? (
+              <li className="navbar_links">
+                <NavLink to="/profile" exact={true} activeClassName="active">
+                  <p className="navbar_link">BuyingPower</p>
+                </NavLink>
+              </li>
+            ) : (
+              ""
+            )}
             {authenticated ? (
               <li className="navbar_links">
                 <NavLink to="/profile" exact={true} activeClassName="active">
